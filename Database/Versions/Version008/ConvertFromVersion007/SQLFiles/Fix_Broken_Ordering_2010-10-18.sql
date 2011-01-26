ac3b025bb8690152eb8f7029f6f0d1a65e066396
mwicks <mwicks>
2011-01-26 11:11:41 +0000

    New Version 8

---
+++ Database/Versions/Version008/ConvertFromVersion007/SQLFiles/Fix_Broken_Ordering_2010-10-18.sql
@@ -0,0 +1,16 @@
+UPDATE ANA_RELATIONSHIP_PROJECT
+SET rlp_sequence = 11
+WHERE rlp_relationship_fk = 31489;
+
+UPDATE ANA_RELATIONSHIP_PROJECT
+SET rlp_sequence = 17
+WHERE rlp_relationship_fk = 32150;
+
+UPDATE ANA_RELATIONSHIP_PROJECT
+SET rlp_sequence = 1
+WHERE rlp_relationship_fk = 33603;
+
+UPDATE ANA_RELATIONSHIP_PROJECT
+SET rlp_sequence = 2
+WHERE rlp_relationship_fk = 33604;
+

bacaf794a5500797527a6ac263378dc1154007f1
mwicks <mwicks>
2012-03-13 11:49:34 +0000

    Useful Java Classes for various standardised operations (pattern matching, executing shell commands, and string reversal); MW - 2012-03-13

---
+++ UsefulJava/src/TestApp/TestString.java
@@ -0,0 +1,40 @@
+package TestApp;
+
+import Utility.WhatIsThisString;
+
+public class TestString {
+
+	public static void main(String args[]){  
+
+		for (int i = 0; i < args.length; i++ ) {
+		    String testString = args[i];
+
+		    if ( WhatIsThisString.isItAValidEmailAddress(testString) ) {
+		    	System.out.println(testString + " is a VALID email address.");  
+		    } 
+		    else if ( WhatIsThisString.isItNumeric(testString) ) {
+		    	System.out.println(testString + " is a VALID Number.");  
+		    } 
+		    else if ( WhatIsThisString.isItNumericWithLeadingSignAndDecimalPoint(testString) ) {
+		    	System.out.println(testString + " is a VALID Decimal Number with a Optional Leading Sign.");  
+		    } 
+		    else if ( WhatIsThisString.isItExponentialNumeric(testString) ) {
+		    	System.out.println(testString + " is a VALID Exponential Number.");  
+		    } 
+		    else if ( WhatIsThisString.isItAValidDate(testString, true) ) {
+		    	System.out.println(testString + " is a VALID UK (Metric) Date.");  
+		    } 
+		    else if ( WhatIsThisString.isItAValidDate(testString, false) ) {
+		    	System.out.println(testString + " is a VALID US (Non-Metric) Date.");  
+		    } 
+		    else if ( WhatIsThisString.isItAValidIPAddress(testString) ) {
+		    	System.out.println(testString + " is a VALID IP Address.");  
+		    } 
+		    else {  
+		    	System.out.println(testString + " is NEITHER Numeric in some way OR an Email Address OR a UK OR US Date OR an IP Address!");  
+		    }  
+		}
+
+	}  
+	
+}
---
+++ UsefulJava/src/Utility/ExecuteCommand.java
@@ -0,0 +1,55 @@
+/*
+ * A Java class to execute shell commands
+ * 
+ * M Wicks, 05/03/2012
+ * 
+ */
+package Utility;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+
+public class ExecuteCommand {
+	
+    public static ArrayList<String> execute(String cmd) {
+
+    	Runtime run = Runtime.getRuntime();
+    	Process pr = null;
+    	
+    	try {
+    		pr = run.exec(cmd);
+    	}
+    	catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	try {
+    		pr.waitFor();
+    	}
+    	catch (InterruptedException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+    	String line = "";
+    	
+    	int i = 0;
+        ArrayList<String> lines = new ArrayList<String>();
+    	
+    	try {
+    		while ((line=buf.readLine())!=null) {
+    			lines.add(line);
+    		}
+    	}
+    	catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return lines;
+    	
+	}
+
+}

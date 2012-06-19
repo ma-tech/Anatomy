a11731a7f2752c19ccc5d62105ac7b76fa4e570a
Mike Wicks <Michael.Wicks@igmm.ed.ac.uk>
2012-06-19 14:41:40 +0100

    New Ajax Call for Searchable Anatomy - for Peter

--- DAOAnatomy008/src/daolayer/DAOProperties.java
+++ DAOAnatomy008/src/daolayer/DAOProperties.java
@@ -47,7 +47,7 @@ import java.util.Properties;
 public class DAOProperties {
 
     // Constants ----------------------------------------------------------------------------------
-    private static final String PROPERTIES_FILE = "dao.properties.mouse";
+    private static final String PROPERTIES_FILE = "dao.properties.qa";
     private static final Properties PROPERTIES = new Properties();
 
     static {
